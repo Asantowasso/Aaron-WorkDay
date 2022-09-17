1ba7216a0d93e3813ce378d2e7cc1d557c0e8a4e
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2022-09-17 18:22:20 -0400

    console log recording text input

--- script.js
+++ script.js
@@ -46,9 +46,17 @@ $(this).click(function(event){
     console.log(event.target.value)
     var text = $(`#text${event.target.value}`).val()
     console.log(text)
+    var time = $(`#time${event.target.value}`).val()
+    localStorage.setItem(time, text);
+
 })
+
+
     
-   
+//$("#8 .description").val(localStorage.getItem("8"));
+//$("#9 .description").val(localStorage.getItem("9"));
+//$("#10 .description").val(localStorage.getItem("10"));
+
     
     })
     
